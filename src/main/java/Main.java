e2123f54f59e10e5a6fc777af21c9884042bf10f
Ianos828 <kokbowen17@gmail.com>
2026-03-05 14:53:42 +0800

    Import Gradle configurations

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class.
+ */
+public class Main {
+    /**
+     * Main method.
+     * @param args optional args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
