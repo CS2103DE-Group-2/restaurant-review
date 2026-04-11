f83deef6e416d95c29f80a4c9b85ae2aebf9df13
Ianos828 <kokbowen17@gmail.com>
2026-04-11 15:13:48 +0800

    Remove deprecated CLI-related parsing methods

--- src/test/java/application/parser/ArgumentParserTest.java
+++ src/test/java/application/parser/ArgumentParserTest.java
@@ -11,52 +11,12 @@ import application.exception.InvalidArgumentException;
 import application.exception.MissingArgumentException;
 
 public class ArgumentParserTest {
-
-    @Test
-    public void splitIntoPair_validInput_success() {
-        String[] result = ArgumentParser.splitIntoPair("command args", " ");
-        assertEquals("command", result[0]);
-        assertEquals("args", result[1]);
-
-        result = ArgumentParser.splitIntoPair("  command   args  ", " ");
-        assertEquals("command", result[0]);
-        assertEquals("args", result[1]);
-    }
-
-    @Test
-    public void splitIntoPair_nullInput_returnsEmptyArray() {
-        String[] result = ArgumentParser.splitIntoPair(null, " ");
-        assertEquals("", result[0]);
-        assertEquals("", result[1]);
-    }
-
-    @Test
-    public void splitIntoPair_noDelimiter_returnsInputAndEmpty() {
-        String[] result = ArgumentParser.splitIntoPair("command", " ");
-        assertEquals("command", result[0]);
-        assertEquals("", result[1]);
-    }
-
     @Test
     public void isValidString_variousInputs() {
         assertTrue(ArgumentParser.isValidString("test"));
         assertFalse(ArgumentParser.isValidString(null));
         assertFalse(ArgumentParser.isValidString(""));
         assertFalse(ArgumentParser.isValidString("   "));
-    }
-
-    @Test
-    public void toInt_validInput_success() throws InvalidArgumentException, MissingArgumentException {
-        assertEquals(123, ArgumentParser.toInt("123"));
-    }
-
-    @Test
-    public void toInt_invalidInput_throwsException() {
-        assertThrows(MissingArgumentException.class, () -> ArgumentParser.toInt(null));
-        assertThrows(MissingArgumentException.class, () -> ArgumentParser.toInt(""));
-        assertThrows(MissingArgumentException.class, () -> ArgumentParser.toInt("   "));
-        assertThrows(InvalidArgumentException.class, () -> ArgumentParser.toInt("abc"));
-        assertThrows(InvalidArgumentException.class, () -> ArgumentParser.toInt("12.34"));
     }
 
     @Test
